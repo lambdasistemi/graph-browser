16cd102dc402929f613fc9404556eca27808371b
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-09 11:06:45 +0100

    test: add ontology import and SPARQL query tests

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Test.OntologyImport as OntologyImport
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
 import Test.QueryCatalog as QueryCatalog
@@ -12,6 +13,7 @@ import Test.SelfGraph as SelfGraph
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
+  OntologyImport.spec
   QueryCatalog.spec
   Sparql.spec
   SelfGraph.spec
